9ec2c9cd772f7545f52a6ee1f66b9979c63ce6ec
Rob Winch <rwinch@gopivotal.com>
2014-07-07 12:26:48 -0500

    Initial

---
+++ src/main/java/demo/SecurityConfig.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/resources/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .openidLogin()
+                .loginPage("/login")
+                .permitAll()
+                .authenticationUserDetailsService(new CustomUserDetailsService())
+                .attributeExchange("https://www.google.com/.*")
+                    .attribute("email")
+                        .type("http://axschema.org/contact/email")
+                        .required(true)
+                        .and()
+                    .attribute("firstname")
+                        .type("http://axschema.org/namePerson/first")
+                        .required(true)
+                        .and()
+                    .attribute("lastname")
+                        .type("http://axschema.org/namePerson/last")
+                        .required(true)
+                        .and()
+                    .and()
+                .attributeExchange(".*yahoo.com.*")
+                    .attribute("email")
+                        .type("http://axschema.org/contact/email")
+                        .required(true)
+                        .and()
+                    .attribute("fullname")
+                        .type("http://axschema.org/namePerson")
+                        .required(true)
+                        .and()
+                    .and()
+                .attributeExchange(".*myopenid.com.*")
+                    .attribute("email")
+                        .type("http://schema.openid.net/contact/email")
+                        .required(true)
+                        .and()
+                    .attribute("fullname")
+                        .type("http://schema.openid.net/namePerson")
+                        .required(true);
+    }
+}
